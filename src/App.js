e10c6285333c5e2b18db0400ee5fa9355f2e1819
Daniel O'Hara <danielohara97@hotmail.co.uk>
2022-01-24 23:00:44 +0000

    Removed further boiletplate code and further setup the About, Projects and Navbar JS files

--- src/App.js
+++ src/App.js
@@ -11,14 +11,12 @@ import Contact from "./components/Contact";
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
+      <p1>hello</p1>
         <Navbar />
         <About />
         <Projects />
         <Skills />
         <Contact />
-        <p1>
-          HELLO
-        </p1>
     </main>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 
+import React from "react";
 export default function Navbar() {
-    
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+    </header>
 }
