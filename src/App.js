3810529f4777a48e9fb64ec5019364b491a29384
Daniel O'Hara <danielohara97@hotmail.co.uk>
2022-01-24 00:32:51 +0000

    Removed more boiletplate files and made changes to the About.js file. Also updated the website favicon and title

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ import Contact from "./components/Contact";
  
 export default function App() {
   return (
-    <main className ="text-gray-400 bg-gray-900 body-font">
+    <main className="text-gray-400 bg-gray-900 body-font">
         <Navbar />
         <About />
         <Projects />
         <Skills />
         <Contact />
-        <text>Hello</text>
+        <p1>
+          HELLO
+        </p1>
     </main>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,10 @@
 
+import React from "react";
+import { projects } from "../data.js";
 export default function Projects() {
-    
+    return (
+        <section>
+
+        </section>
+    );
 }
