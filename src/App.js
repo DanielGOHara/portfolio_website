f7580d42e489d3d9e6d0a30e18a31b1a2d538b87
Daniel O'Hara <danielohara97@hotmail.co.uk>
2022-06-10 23:40:34 +0100

    Added javascript file extensions to navBar.js & infoSection.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 // Import Necessary Components for the Navigation Bar
 
 import React from "react";
-import Navbar from "./components/navBar";
-import InfoSection from "./components/infoSection"
+import Navbar from "./components/navBar.js";
+import InfoSection from "./components/infoSection.js"
  
 export default function App() {
   return (
