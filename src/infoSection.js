b0b4f45127b717393a2cc201b3539180dcf1139a
Daniel O'Hara <danielohara97@hotmail.co.uk>
2022-06-12 18:55:00 +0100

    Replaced test contents with Work In Progress... so it is clearer the site is being worked on

--- src/infoSection.js
+++ src/infoSection.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function infoSection() {
     return (
       <section id="infoSection">
-        <span id="contents">Test Contents</span>
+        <span id="contents">Work In Progress...</span>
       </section>
       );
 }
