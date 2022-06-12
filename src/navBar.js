ff943b912303213a0d972a41bea4317f49b1c841
Daniel O'Hara <danielohara97@hotmail.co.uk>
2022-06-12 18:42:31 +0100

    Removed old images and added an updated website logo jpg file. Adjusted CSS and added space for logo and statement on left side of the navBar.

--- src/navBar.js
+++ src/navBar.js
@@ -7,12 +7,16 @@ import Skills from "./components/navBar_components/Skills.js";
 import Contact from "./components/navBar_components/Contact.js";
 export default function Navbar() {
     return (
-      <section id="navBar">
+      <nav id="navBar">
+          <section id="logoStatementContainer">
+              <img id="pageLogo" alt ="Logo" src="./components/media/logo.jpg" height="85px" width="85px"/>
+              <span id="pageStatement">Site Developed By Me.</span>
+          </section>
           <Home/>
           <Projects/>
           <Skills/>
           <About/>
           <Contact/>
-      </section>
+      </nav>
       );
 }
